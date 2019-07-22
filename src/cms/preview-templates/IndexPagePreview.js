9e986429e0ce5687f1d4186a1d3e72658cc59ea8
Gatsby <gatsby@The-Great.local>
2019-07-22 18:55:57 +0200

    edits config file + CMS preview templates

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -15,6 +15,8 @@ const IndexPagePreview = ({ entry, getAsset }) => {
         description={data.description}
         intro={data.intro || { blurbs: [] }}
         mainpitch={data.mainpitch || {}}
+        paragraph={data.paragraph}
+        multipleItems={data.multipleItems}
       />
     )
   } else {
